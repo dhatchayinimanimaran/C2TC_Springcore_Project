4c75a718d1534eb29a2b7df1f23ca8288bd7c514
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-11-05 11:06:43 +0530

    commit

---
+++ springdemo/src/main/java/com/tnsif/springdemo/App.java
@@ -0,0 +1,13 @@
+package com.tnsif.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class App {
+		public static void main(String[] args) {
+			
+			ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+			Mobile mobile = context.getBean("mobile", Mobile.class);
+			mobile.call();
+			mobile.data();
+		}
+}
